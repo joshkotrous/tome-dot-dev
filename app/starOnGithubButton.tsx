e692975f2271ea88d0d2b25011b6149b946d975c
joshkotrous <joshkotrous@gmail.com>
2025-06-25 23:17:56 -0400

    Added analytics

--- app/starOnGithubButton.tsx
+++ app/starOnGithubButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { track } from "@vercel/analytics";
+import Link from "next/link";
 
 export default function StarOnGithub({
   width = 60,
@@ -10,12 +11,17 @@ export default function StarOnGithub({
   height?: number;
 }) {
   return (
-    <iframe
+    <Link
+      href="https://github.com/joshkotrous/tome"
       onClick={() => track(`Star on Github`)}
-      src="https://ghbtns.com/github-btn.html?user=joshkotrous&repo=tome&type=star&size=medium"
-      width={width}
-      height={height}
-      title="GitHub"
-    ></iframe>
+      target="_blank"
+    >
+      <iframe
+        src="https://ghbtns.com/github-btn.html?user=joshkotrous&repo=tome&type=star&size=medium"
+        width={width}
+        height={height}
+        title="GitHub"
+      ></iframe>
+    </Link>
   );
 }
